55fab0872da03222d51b2e4304737ca50e1444f1
Akimutina <120643609+Akimutina@users.noreply.github.com>
2023-06-17 15:11:07 +0700

    Задача 1 и 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    return arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+    return users.filter(user => user.gender === gender)
+    .map(user => user.age)
+    .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
 }
